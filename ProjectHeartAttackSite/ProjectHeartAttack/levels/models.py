d077e04df2e389717f69ff1f5cb0b820515866e2
SylvesterFox <51517881+SylvesterFox@users.noreply.github.com>
2021-07-12 21:14:53 +0300

    Updated10721B

--- ProjectHeartAttackSite/ProjectHeartAttack/levels/models.py
+++ ProjectHeartAttackSite/ProjectHeartAttack/levels/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 
 class Levels(models.Model):
     _EASY = "Easy"
@@ -15,16 +18,21 @@ class Levels(models.Model):
         (_EXPERTPLUS, "Expert+"),
     ]
 
-    name_level = models.CharField(max_length=120, verbose_name='Name levels')
+    name_level = models.CharField(max_length=20, verbose_name='Name levels')
     rating = models.IntegerField(default=0, verbose_name='Reating')
-    autthor_level = models.CharField(max_length=80, verbose_name='Author') # Времмено побудет charfield
+    author_level = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Author', on_delete=models.CASCADE) 
     diffculty = models.CharField(choices=_DIFFCULTY, max_length=20, verbose_name='Diffcuty')
-    content = models.TextField(verbose_name='Content')
-    levels_file = models.FileField(upload_to='levels_file/%Y/%m/%d/', verbose_name="Levels file")
+    content = models.TextField(verbose_name='Content', max_length=1024)
+    levels_file = models.FileField(upload_to='levels_file/%Y/%m/%d/', verbose_name="Levels file", blank=True) # не забыть поставить blank=false после дебага
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
     update_at = models.DateTimeField(auto_now=True, verbose_name="Update date")
     preview = models.ImageField(upload_to='preview_levels/%Y/%m/%d/', verbose_name='Preview image', blank=True)
+    level_by = models.CharField(max_length=150, verbose_name="Level By", null=True)
+    url_getsong = models.CharField(max_length=1000, verbose_name="Song url", null=True, blank=True)
+    url_steam_workshop_author = models.CharField(max_length=1000, verbose_name='Creator Workshop', null=True, blank=True)
+    follow_artist = models.CharField(max_length=1000, verbose_name='Follow Artist', null=True, blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Published')
+    comments = GenericRelation('comment')
 
     def __str__(self):
         return self.name_level
@@ -34,15 +42,29 @@ class Levels(models.Model):
         verbose_name_plural = 'Levels database'
         ordering = ['-created_at']
 
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Author comment", on_delete=models.CASCADE)
+    context = models.TextField(verbose_name='Text comment')
+    parent = models.ForeignKey(
+        'self',
+        verbose_name='Parent comment',
+        blank=True,
+        null=True,
+        related_name='comment_children',
+        on_delete=models.CASCADE,
+    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    like_count = models.IntegerField(default=0, verbose_name="Like")
+    Dislike_count = models.IntegerField(default=0, verbose_name="Dislike")
+    heart_comments = models.BooleanField(default=False, verbose_name="Heart")
+    timestamp = models.DateTimeField(auto_now=True, verbose_name="Date craeted comments")
 
-# id - INT
-# level_name - Varchar
-# rating - int
-# autthor_level - varchar
-# diffculty - charField
-# content - Text
-# file - file
-# created_at - datatime
-# update_at - datatime
-# preview - Image
-# is_published - Boolean
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+
--- ProjectHeartAttackSite/ProjectHeartAttack/manage.py
+++ ProjectHeartAttackSite/ProjectHeartAttack/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectHeartAttack.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectHeartAttackUpdated10721.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
